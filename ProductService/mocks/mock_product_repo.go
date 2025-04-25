24d90260c5692dc998ea50594e2f50272d2b5e28
DavidBalazic <davidbalazic31@hotmail.com>
2025-04-25 16:39:07 +0200

    feat: added new method tests

--- ProductService/mocks/mock_product_repo.go
+++ ProductService/mocks/mock_product_repo.go
@@ -55,49 +55,19 @@ func (mr *MockProductRepositoryMockRecorder) AddProduct(ctx, product any) *gomoc
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "AddProduct", reflect.TypeOf((*MockProductRepository)(nil).AddProduct), ctx, product)
 }
 
-// FindAllProductPrices mocks base method.
-func (m *MockProductRepository) FindAllProductPrices(ctx context.Context, name string) ([]models.Product, error) {
+// AddProducts mocks base method.
+func (m *MockProductRepository) AddProducts(ctx context.Context, products []models.Product) ([]models.Product, error) {
 	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "FindAllProductPrices", ctx, name)
+	ret := m.ctrl.Call(m, "AddProducts", ctx, products)
 	ret0, _ := ret[0].([]models.Product)
 	ret1, _ := ret[1].(error)
 	return ret0, ret1
 }
 
-// FindAllProductPrices indicates an expected call of FindAllProductPrices.
-func (mr *MockProductRepositoryMockRecorder) FindAllProductPrices(ctx, name any) *gomock.Call {
+// AddProducts indicates an expected call of AddProducts.
+func (mr *MockProductRepositoryMockRecorder) AddProducts(ctx, products any) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "FindAllProductPrices", reflect.TypeOf((*MockProductRepository)(nil).FindAllProductPrices), ctx, name)
-}
-
-// FindCheapestProduct mocks base method.
-func (m *MockProductRepository) FindCheapestProduct(ctx context.Context, name string) (models.Product, error) {
-	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "FindCheapestProduct", ctx, name)
-	ret0, _ := ret[0].(models.Product)
-	ret1, _ := ret[1].(error)
-	return ret0, ret1
-}
-
-// FindCheapestProduct indicates an expected call of FindCheapestProduct.
-func (mr *MockProductRepositoryMockRecorder) FindCheapestProduct(ctx, name any) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "FindCheapestProduct", reflect.TypeOf((*MockProductRepository)(nil).FindCheapestProduct), ctx, name)
-}
-
-// FindCheapestProductByStore mocks base method.
-func (m *MockProductRepository) FindCheapestProductByStore(ctx context.Context, name, store string) (models.Product, error) {
-	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "FindCheapestProductByStore", ctx, name, store)
-	ret0, _ := ret[0].(models.Product)
-	ret1, _ := ret[1].(error)
-	return ret0, ret1
-}
-
-// FindCheapestProductByStore indicates an expected call of FindCheapestProductByStore.
-func (mr *MockProductRepositoryMockRecorder) FindCheapestProductByStore(ctx, name, store any) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "FindCheapestProductByStore", reflect.TypeOf((*MockProductRepository)(nil).FindCheapestProductByStore), ctx, name, store)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "AddProducts", reflect.TypeOf((*MockProductRepository)(nil).AddProducts), ctx, products)
 }
 
 // FindProductById mocks base method.
@@ -113,4 +83,19 @@ func (m *MockProductRepository) FindProductById(ctx context.Context, id string) 
 func (mr *MockProductRepositoryMockRecorder) FindProductById(ctx, id any) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "FindProductById", reflect.TypeOf((*MockProductRepository)(nil).FindProductById), ctx, id)
+}
+
+// FindProductsByIds mocks base method.
+func (m *MockProductRepository) FindProductsByIds(ctx context.Context, ids []string) ([]models.Product, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "FindProductsByIds", ctx, ids)
+	ret0, _ := ret[0].([]models.Product)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// FindProductsByIds indicates an expected call of FindProductsByIds.
+func (mr *MockProductRepositoryMockRecorder) FindProductsByIds(ctx, ids any) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "FindProductsByIds", reflect.TypeOf((*MockProductRepository)(nil).FindProductsByIds), ctx, ids)
 }
